91605cae66daa4accaba4143242c4678330a08d7
massi <massimo.battinelli@gmail.com>
2022-03-03 16:47:26 +0100

    manca funzione bomba

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-const areaSquare = document.querySelector(".cont-square");
 
-for (let i = 1; i <= 100; i++) {
-    let numero = i;
-    let backcol = 'colorcells'
+const eleLevel = document.getElementById('level');
+const btnPlay = document.getElementById('play');
+const eleGrid = document.querySelector('.grid');
+const arrLevels = [144, 81, 49];
+let bombs;
 
-    areaSquare.innerHTML += `
-        <div class="box ${backcol}">${numero}</div>`;
+btnPlay.addEventListener('click', setupGame);
+
+
+function setupGame() {
+	eleGrid.innerHTML = '';
+
+	const indexLevel = parseInt(eleLevel.value);
+	const cellsCount = arrLevels[indexLevel];
+	const cellsPerRow = Math.sqrt(cellsCount);
+
+	for (let cellNum = 1; cellNum <= cellsCount; cellNum++){
+		const eleCell = document.createElement('div');
+		eleCell.classList.add('cell');
+		eleCell.innerHTML = cellNum;
+		eleCell.style.width = `calc(100% / ${cellsPerRow})`;
+		eleCell.style.height = `calc(100% / ${cellsPerRow})`;
+		eleCell.addEventListener('click', changeCellColor);
+		eleGrid.append(eleCell);
+	}
 }
 
-let difficultyElem = document.getElementById("nav2");
-let playBtn = document.getElementById("button-play");
-let cellNumber = [49,81,100];
-let bombs;
+function changeCellColor(event) {
+	this.classList.add('selected');
+}
 
